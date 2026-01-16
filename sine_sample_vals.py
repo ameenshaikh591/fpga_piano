5a767187eed3d9a8d0c310109b45c673f5e0dd43
Ameen Shaikh <ameenshaikh591@gmail.com>
2026-01-16 16:49:18 -0500

    Update Python script for graph

--- sine_sample_vals.py
+++ sine_sample_vals.py
@@ -9,20 +9,39 @@ MAX_PWM = 255
 
 sine_sample_vals = []
 
-for x in range(NUM_SAMPLES):
-    phase = (2 * math.pi * x) / NUM_SAMPLES
-    sample_value = int(MAX_PWM * ((math.sin(phase) + 1) / 2))
-    sine_sample_vals.append(sample_value)
+def generate_samples():
+    global sine_sample_vals
+    
+    for x in range(NUM_SAMPLES):
+        phase = (2 * math.pi * x) / NUM_SAMPLES
+        sample_value = int(MAX_PWM * ((math.sin(phase) + 1) / 2))
+        sine_sample_vals.append(sample_value)
 
-plt.figure()
-plt.plot(sine_sample_vals)
-plt.xlabel("Sample index")
-plt.ylabel("PWM value")
-plt.title("128-sample PWM sine wave")
-plt.grid(True)
-plt.show()
+def plot_samples():
+    global sine_sample_vals
+    
+    plt.figure()
+    plt.plot(sine_sample_vals)
+    plt.xlabel("Sample index")
+    plt.ylabel("PWM value")
+    plt.title("128-sample PWM sine wave")
+    plt.grid(True)
+    plt.show()
 
+def print_samples():
+    global sine_sample_vals
+    
+    for samples in sine_sample_vals:
+        print(samples)
 
+def main():
+    global sine_sample_vals
+
+    generate_samples()
+    #plot_samples()
+    print_samples()
+
+main()
 
 
     
